06b64c98ccfd926dc463956a81e3941dd54fc760
diksham10 <shresthadiksham@gmail.com>
2024-06-06 08:18:33 +0545

    day 4

---
+++ stringmethods.js
@@ -0,0 +1,16 @@
+var text="I am currently involved in Bsc. CSIT 5th semester";
+//alert(text);
+console.log(text);
+
+//print the length if the string
+console.log("the length of the text is:",text.length);
+
+//print the character at index 5 of the given string
+console.log("the character at index 5 is:",text.charAt(5));
+
+//print the character code at index 5 of the given string
+console.log("the character code at index 5 is:",text.charCodeAt(5));
+
+//print and write it into the document,the text between 3 and 10 index
+console.log("the text between 3 and 10 index is:",text.slice(3,10));
+document.write("the text between 3 and 10 index is =>")
