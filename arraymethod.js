1392c24126628a34c1711086661e8a8ac59a5287
Ranjuuu <ranjusanjel1@gmail.com>
2024-06-09 08:28:57 +0545

    Add files via upload

---
+++ arraymethod.js
@@ -0,0 +1,12 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let size = fruits.length;
+console.log(`The length of the given array is ${size}`);
+
+//array to string
+ console.log(`The string form of the given array is ${fruits.toString()}`);
+console.log(`The string of the given array is ${size}`);
+
+//array to arrayat
+console.log(`The string form of the given array is ${fruits.at(2)}`);
+
+//
---
+++ numbermethod.js
@@ -0,0 +1,17 @@
+var xyz=1214234;
+
+//convert number to string
+
+console.log("converting number to string",xyz.toString());
+ 
+//convert number to exponential
+console.log("converting number to string",xyz.toExponential(2));
+
+//convert number to fixed method
+console.log("converting number to string",xyz.toFixed(3));
+
+//convert number to precision
+console.log("converting number to string",xyz.toPrecision());
+
+//convert to valueof
+console.log("converting number to string",xyz.valueOf());
