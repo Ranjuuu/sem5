84354cb915aec5a549435a427d6ae8580907dada
Ranjuuu <ranjusanjel1@gmail.com>
2024-06-18 08:12:57 +0545

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,20 @@
+//show if the number is greater than or less than 30 using if else if else in javascript
+
+var num= 25;
+
+if(num>30)
+    {
+        console.log(`the number is greater than 30`)
+    }
+    else if(num<30){
+        console.log(`the number is less than 30`)
+    }
+    else{
+        console.log(`the number is equal to 30`)
+    }
+    
+
+    //show if the number is odd or even using if and if else in javascript
+    const num= 7;
+
+    if(num%==2)
